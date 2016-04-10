994dc62cf90041da85c37a03d80389afd586371d
Konstantin Shchepanovskyi <schepanovsky@gmail.com>
2016-04-10 11:30:12 +0300

    Add commenter: comment a line or block of code using `Ctrl+/` or `Ctrl+Shift+/`

---
+++ src/main/java/io/protostuff/jetbrains/plugin/ProtoCommenter.java
@@ -0,0 +1,33 @@
+package io.protostuff.jetbrains.plugin;
+
+import com.intellij.lang.Commenter;
+
+/**
+ * @author Kostiantyn Shchepanovskyi
+ */
+public class ProtoCommenter implements Commenter {
+
+    public static final String LINE_COMMENT_PREFIX = "//";
+    public static final String BLOCK_COMMENT_PREFIX = "/*";
+    public static final String BLOCK_COMMENT_SUFFIX = "*/";
+
+    public String getLineCommentPrefix() {
+        return LINE_COMMENT_PREFIX;
+    }
+
+    public String getBlockCommentPrefix() {
+        return BLOCK_COMMENT_PREFIX;
+    }
+
+    public String getBlockCommentSuffix() {
+        return BLOCK_COMMENT_SUFFIX;
+    }
+
+    public String getCommentedBlockCommentPrefix() {
+        return null;
+    }
+
+    public String getCommentedBlockCommentSuffix() {
+        return null;
+    }
+}
