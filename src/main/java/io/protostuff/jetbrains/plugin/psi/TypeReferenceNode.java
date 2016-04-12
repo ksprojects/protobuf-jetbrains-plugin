0f57f1687b74314a79113c74a68087b6d7c275bb
Konstantin Shchepanovskyi <schepanovsky@gmail.com>
2016-04-12 22:03:28 +0300

    Refactoring

--- src/main/java/io/protostuff/jetbrains/plugin/psi/TypeReferenceNode.java
+++ src/main/java/io/protostuff/jetbrains/plugin/psi/TypeReferenceNode.java
@@ -1,29 +1,16 @@
 package io.protostuff.jetbrains.plugin.psi;
 
 import com.intellij.lang.ASTNode;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiNamedElement;
-import io.protostuff.jetbrains.plugin.ProtoParserDefinition;
-import org.antlr.jetbrains.adapter.psi.IdentifierDefSubtree;
-import org.antlr.jetbrains.adapter.psi.ScopeNode;
+import org.antlr.jetbrains.adapter.psi.ANTLRPsiNode;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 /**
  * @author Kostiantyn Shchepanovskyi
  */
-public class TypeReferenceNode
-        extends IdentifierDefSubtree
-        implements ScopeNode, KeywordsContainer {
+public class TypeReferenceNode extends ANTLRPsiNode implements KeywordsContainer {
 
     public TypeReferenceNode(@NotNull ASTNode node) {
-        super(node, ProtoParserDefinition.ID);
-    }
-
-    @Nullable
-    @Override
-    public PsiElement resolve(PsiNamedElement element) {
-        return null;
+        super(node);
     }
 
 }
