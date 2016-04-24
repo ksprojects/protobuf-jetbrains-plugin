2a57c4435b96d531666beabc4177ac5e11e0e29d
Konstantin Shchepanovskyi <schepanovsky@gmail.com>
2016-04-24 14:34:57 +0300

    Add tests for parsing and PSI structure

---
+++ src/test/java/io/protostuff/jetbrains/plugin/parsing/ParsingTest.java
@@ -0,0 +1,46 @@
+package io.protostuff.jetbrains.plugin.parsing;
+
+import com.intellij.testFramework.ParsingTestCase;
+import io.protostuff.jetbrains.plugin.ProtoParserDefinition;
+import org.junit.Test;
+
+/**
+ * @author Kostiantyn Shchepanovskyi
+ */
+public class ParsingTest extends ParsingTestCase {
+
+    public ParsingTest() {
+        super("parsing", "proto", new ProtoParserDefinition());
+    }
+
+    public void testMessage() {
+        doTest(true);
+    }
+
+    public void testEnum() {
+        doTest(true);
+    }
+
+    public void testService() {
+        doTest(true);
+    }
+
+    public void testHeaderAndFooterComments() {
+        doTest(true);
+    }
+
+    @Override
+    protected String getTestDataPath() {
+        return "src/test/resources";
+    }
+
+    @Override
+    protected boolean skipSpaces() {
+        return false;
+    }
+
+    @Override
+    protected boolean includeRanges() {
+        return true;
+    }
+}
