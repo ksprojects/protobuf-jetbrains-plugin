2787d8cb9ef2a9588c0495981b402019723137e7
Konstantin Shchepanovskyi <schepanovsky@gmail.com>
2016-05-09 22:50:25 +0300

    #15: Highlighting of groups
    Fixed https://github.com/protostuff/protostuff-jetbrains-plugin/issues/15

--- src/main/java/io/protostuff/jetbrains/plugin/psi/GroupNode.java
+++ src/main/java/io/protostuff/jetbrains/plugin/psi/GroupNode.java
@@ -1,8 +1,15 @@
 package io.protostuff.jetbrains.plugin.psi;
 
 import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
 import org.antlr.jetbrains.adapter.psi.ANTLRPsiNode;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static io.protostuff.jetbrains.plugin.ProtoParserDefinition.R_FIELD_MODIFIER;
 
 /**
  * @author Kostiantyn Shchepanovskyi
@@ -13,4 +20,15 @@ public class GroupNode extends ANTLRPsiNode implements KeywordsContainer {
         super(node);
     }
 
+    @Override
+    public Collection<PsiElement> keywords() {
+        ASTNode node = getNode();
+        List<PsiElement> result = new ArrayList<>();
+        result.addAll(Util.findKeywords(getNode()));
+        ASTNode fieldModifier = node.findChildByType(R_FIELD_MODIFIER);
+        if (fieldModifier != null) {
+            result.addAll(Util.findKeywords(fieldModifier));
+        }
+        return result;
+    }
 }
