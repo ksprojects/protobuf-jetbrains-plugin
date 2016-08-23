6b964338794a9b181dd33b2dac4565391ad7e9e2
Konstantin Shchepanovskyi <schepanovsky@gmail.com>
2016-08-23 21:59:53 +0300

    #8: Detect and disable conflicting plugins
    Use FileTypeFactory to detect other plugins that can open *.proto files.

--- src/main/java/io/protostuff/jetbrains/plugin/ProtostuffPluginController.java
+++ src/main/java/io/protostuff/jetbrains/plugin/ProtostuffPluginController.java
@@ -2,32 +2,31 @@ package io.protostuff.jetbrains.plugin;
 
 import com.intellij.ide.plugins.IdeaPluginDescriptor;
 import com.intellij.ide.plugins.PluginManager;
-import com.intellij.ide.plugins.cl.PluginClassLoader;
-import com.intellij.lang.Language;
 import com.intellij.notification.NotificationGroup;
 import com.intellij.notification.NotificationType;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.extensions.ExtensionPoint;
+import com.intellij.openapi.extensions.Extensions;
 import com.intellij.openapi.extensions.PluginId;
-import com.intellij.openapi.fileTypes.LanguageFileType;
+import com.intellij.openapi.fileTypes.*;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.SystemInfo;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.event.HyperlinkEvent;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
+import static com.intellij.openapi.fileTypes.FileTypeFactory.FILE_TYPE_FACTORY_EP;
+
 public class ProtostuffPluginController implements ProjectComponent {
+
+    private static final String PLUGIN_NAME = "Protobuf Support";
     private static final String PLUGIN_ID = "io.protostuff.protostuff-jetbrains-plugin";
-
     private static final Logger LOGGER = Logger.getInstance(ProtostuffPluginController.class);
-    public static final String PLUGIN_NAME = "Protobuf Support";
-
     private Project project;
 
     public ProtostuffPluginController(Project project) {
@@ -42,59 +41,47 @@ public class ProtostuffPluginController implements ProjectComponent {
 
     @Override
     public void projectOpened() {
-        LOGGER.info("projectOpened " + project.getName());
-        IdeaPluginDescriptor plugin = PluginManager.getPlugin(PluginId.getId(PLUGIN_ID));
-        String version = "unknown";
-        if (plugin != null) {
-            version = plugin.getVersion();
-        }
-        LOGGER.info("Protostuff Plugin version " + version + ", Java version " + SystemInfo.JAVA_VERSION);
         try {
-            tryDisableConflictingPlugins();
+            checkConflictingPlugins();
         } catch (Exception e) {
             LOGGER.error("Could not detect or disable conflicting plugins", e);
         }
     }
 
-    private void tryDisableConflictingPlugins() {
-        List<IdeaPluginDescriptor> conflictingPlugins = new ArrayList<>();
-        Collection<Language> languages = Language.getRegisteredLanguages();
-        for (Language language : languages) {
-            LanguageFileType associatedFileType = language.getAssociatedFileType();
-            if (associatedFileType != null && associatedFileType != ProtoFileType.INSTANCE) {
-                String extension = associatedFileType.getDefaultExtension();
-                if (ProtoFileType.FILE_EXTENSION.equals(extension)) {
-                    // TODO: How to match language to plugin? Is there a better way?
-                    // https://github.com/protostuff/protobuf-jetbrains-plugin/issues/8
-                    Class<? extends LanguageFileType> clazz = associatedFileType.getClass();
-                    PluginClassLoader classLoader = (PluginClassLoader) clazz.getClassLoader();
-                    PluginId pluginId = classLoader.getPluginId();
-                    IdeaPluginDescriptor conflict = PluginManager.getPlugin(pluginId);
-                    if (conflict != null && conflict.isEnabled()) {
-                        LOGGER.warn("Detected conflicting plugin for *.proto files: " + conflict.getName() + " (" + pluginId + ")");
-                        conflictingPlugins.add(conflict);
-                    }
-                }
-            }
-        }
+    private void checkConflictingPlugins() {
+        FileTypeUtil fileTypeUtil = new FileTypeUtil();
+        Collection<IdeaPluginDescriptor> conflictingPlugins = fileTypeUtil.getPluginsForFile("file.proto");
         if (!conflictingPlugins.isEmpty()) {
-            String conflictList = String.join("\n", conflictingPlugins.stream()
-                    .map(IdeaPluginDescriptor::getName)
-                    .map(name -> "<li>" + name + "</li>")
-                    .collect(Collectors.toList()));
-            final String text = ProtostuffBundle.message("action.disable.conflicting.plugins", conflictList);
-            NotificationGroup ng = NotificationGroup.balloonGroup("Conflicting Plugins");
-            ng.createNotification(PLUGIN_NAME, text, NotificationType.WARNING,
-                    (notification, event) -> {
-                        if (event.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
-                            for (IdeaPluginDescriptor foreignPlugin : conflictingPlugins) {
-                                PluginManager.disablePlugin(foreignPlugin.getPluginId().toString());
-                            }
-                            Application application = ApplicationManager.getApplication();
-                            application.restart();
-                        }
-                    }).notify(project);
+            askUserToDisablePlugins(conflictingPlugins);
         }
+    }
+
+    private void askUserToDisablePlugins(Collection<IdeaPluginDescriptor> conflictingPlugins) {
+        final String text = formatMessage(conflictingPlugins);
+        NotificationGroup ng = NotificationGroup.balloonGroup("Conflicting Plugins");
+        ng.createNotification(PLUGIN_NAME, text, NotificationType.WARNING,
+                (notification, event) -> {
+                    if (event.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+                        for (IdeaPluginDescriptor foreignPlugin : conflictingPlugins) {
+                            PluginManager.disablePlugin(foreignPlugin.getPluginId().toString());
+                        }
+                        Application application = ApplicationManager.getApplication();
+                        application.restart();
+                    }
+                }).notify(project);
+    }
+
+    @NotNull
+    private String formatMessage(Collection<IdeaPluginDescriptor> conflictingPlugins) {
+        String conflictList = formatHtmlPluginList(conflictingPlugins);
+        return ProtostuffBundle.message("action.disable.conflicting.plugins", conflictList);
+    }
+
+    private String formatHtmlPluginList(Collection<IdeaPluginDescriptor> conflictingPlugins) {
+        return String.join("\n", conflictingPlugins.stream()
+                .map(IdeaPluginDescriptor::getName)
+                .map(name -> "<li>" + name + "</li>")
+                .collect(Collectors.toList()));
     }
 
     @Override
@@ -109,5 +96,99 @@ public class ProtostuffPluginController implements ProjectComponent {
     @Override
     public String getComponentName() {
         return "ProtostuffPluginController";
+    }
+
+    private static class FileTypeUtil {
+
+        private final IdeaPluginDescriptor self = PluginManager.getPlugin(PluginId.getId(PLUGIN_ID));
+        private final Map<String, FileTypeEntry> fileTypes = new LinkedHashMap<>();
+
+        FileTypeUtil() {
+            ExtensionPoint<FileTypeFactory> ep = Extensions.getRootArea().getExtensionPoint(FILE_TYPE_FACTORY_EP);
+            for (FileTypeFactory typeFactory : ep.getExtensions()) {
+                typeFactory.createFileTypes(new FileTypeConsumer() {
+                    @Override
+                    public void consume(@NotNull FileType fileType) {
+                        register(fileType, parse(fileType.getDefaultExtension()));
+                    }
+
+                    @Override
+                    public void consume(@NotNull final FileType fileType, String extensions) {
+                        register(fileType, parse(extensions));
+                    }
+
+                    @Override
+                    public void consume(@NotNull final FileType fileType, @NotNull final FileNameMatcher... matchers) {
+                        register(fileType, new ArrayList<>(Arrays.asList(matchers)));
+                    }
+
+                    @Override
+                    public FileType getStandardFileTypeByName(@NotNull final String name) {
+                        FileTypeEntry type = fileTypes.get(name);
+                        return type != null ? type.fileType : null;
+                    }
+
+                    private void register(@NotNull FileType fileType, @NotNull List<FileNameMatcher> fileNameMatchers) {
+                        FileTypeEntry type = fileTypes.get(fileType.getName());
+                        if (type != null) {
+                            type.matchers.addAll(fileNameMatchers);
+                        } else {
+                            fileTypes.put(fileType.getName(), new FileTypeEntry(fileType, fileNameMatchers));
+                        }
+                    }
+                });
+            }
+        }
+
+        @NotNull
+        private List<FileNameMatcher> parse(@Nullable String semicolonDelimited) {
+            if (semicolonDelimited == null) {
+                return Collections.emptyList();
+            }
+
+            StringTokenizer tokenizer = new StringTokenizer(semicolonDelimited, FileTypeConsumer.EXTENSION_DELIMITER, false);
+            ArrayList<FileNameMatcher> list = new ArrayList<>();
+            while (tokenizer.hasMoreTokens()) {
+                list.add(new ExtensionFileNameMatcher(tokenizer.nextToken().trim()));
+            }
+            return list;
+        }
+
+        Collection<IdeaPluginDescriptor> getPluginsForFile(String filename) {
+            Map<PluginId, IdeaPluginDescriptor> plugins = new HashMap<>();
+            fileTypes.values().forEach(type -> {
+                if (type.match(filename)) {
+                    Class<? extends FileType> fileTypeClass = type.fileType.getClass();
+                    PluginId pluginId = PluginManager.getPluginByClassName(fileTypeClass.getName());
+                    if (!Objects.equals(self.getPluginId(), pluginId)) {
+                        plugins.put(pluginId, PluginManager.getPlugin(pluginId));
+                    }
+                }
+            });
+            return plugins.values();
+        }
+
+        static class FileTypeEntry {
+            @NotNull
+            private final FileType fileType;
+            @NotNull
+            private final List<FileNameMatcher> matchers;
+
+            private FileTypeEntry(@NotNull FileType fileType, @NotNull List<FileNameMatcher> matchers) {
+                this.fileType = fileType;
+                this.matchers = matchers;
+            }
+
+            boolean match(String filename) {
+                for (FileNameMatcher matcher : matchers) {
+                    if (matcher.accept(filename)) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+
+
     }
 }
