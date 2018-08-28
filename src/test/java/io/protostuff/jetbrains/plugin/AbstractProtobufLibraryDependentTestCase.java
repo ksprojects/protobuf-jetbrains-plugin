f5078318167000c5a57741e321d8e5d3be8e6790
Konstantin Shchepanovskyi <schepanovsky@gmail.com>
2018-08-29 00:38:59 +0300

    104: Dumb fix for test failure

--- src/test/java/io/protostuff/jetbrains/plugin/AbstractProtobufLibraryDependentTestCase.java
+++ src/test/java/io/protostuff/jetbrains/plugin/AbstractProtobufLibraryDependentTestCase.java
@@ -1,18 +1,32 @@
 package io.protostuff.jetbrains.plugin;
 
-import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
+import com.intellij.testFramework.LightProjectDescriptor;
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
 
 /**
  * Parent for tests that depend on global protobuf library installed for a module.
  *
  * @author Kostiantyn Shchepanovskyi
  */
-public abstract class AbstractProtobufLibraryDependentTestCase extends LightCodeInsightFixtureTestCase {
+public abstract class AbstractProtobufLibraryDependentTestCase extends LightPlatformCodeInsightFixtureTestCase {
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
         ProtostuffPluginController controller = myFixture.getProject().getComponent(ProtostuffPluginController.class);
         controller.addLibrary(myModule);
+    }
+
+    /**
+     * Dumb fix for test failure:
+     * ./gradlew test --debug --tests *Test.testUnresolvedOptionReference_StandardOption
+     * --tests FindUsagesTest.testStandardOptionReferenceUsages
+     * When FindUsagesTest does not run first - it fails.
+     */
+    @Override
+    protected LightProjectDescriptor getProjectDescriptor() {
+        return new LightProjectDescriptor() {
+
+        };
     }
 }
