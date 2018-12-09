b4fa0e9b7582acb09e4bd83c13587ce58f1a4159
Kostiantyn Shchepanovskyi <schepanovsky@gmail.com>
2018-12-09 23:24:16 +0200

    #127: Add plugin icon
    http://www.jetbrains.org/intellij/sdk/docs/basics/plugin_structure/plugin_icon_file.html

--- src/main/icons-src/convert.sh
+++ src/main/icons-src/convert.sh
@@ -9,15 +9,18 @@ function convertXcf {
     image=$1
     echo "Converting ${image}"
     convert -alpha Set -background none -layers merge "${image}.xcf" "${dst}/${image}@tmp.png"
+    convert -alpha Set -background none -layers merge "${image}.xcf" "${dst}/${image}.svg"
     convert -resize 32x32 "${dst}/${image}@tmp.png" "${dst}/${image}@2x.png"
     convert -resize 16x16 "${dst}/${image}@tmp.png" "${dst}/${image}.png"
     rm "${dst}/${image}@tmp.png"
 }
 
 
-rm -rvf $dst
+rm -r $dst
 mkdir $dst
 
+convertXcf 'pluginIcon'
+convertXcf 'pluginIcon_dark'
 convertXcf 'proto'
 convertXcf 'proto_dark'
 convertXcf 'message'
